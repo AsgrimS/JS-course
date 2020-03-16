97b419655ab254c0a7b41792f19a5f4832729794
AsgrimS <jan.semik@gmail.com>
2020-03-16 17:28:02 +0100

    .

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -2,6 +2,8 @@ const button = document.querySelector('button');
 
 const myUl = document.querySelector('ul');
 
+const title = document.querySelector('header');
+
 myUl.className = 'test';
 
 button.addEventListener('click', () => {
