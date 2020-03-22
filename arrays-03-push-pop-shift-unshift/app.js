94b6d81a61e12499464858d20524464f4cab3f5a
Asgrim_S <jan.semik@gmail.com>
2020-03-22 18:48:34 +0100

    arrays

---
+++ arrays-03-push-pop-shift-unshift/app.js
@@ -0,0 +1,41 @@
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+
+// // const moreNumbers = Array(5, 2);
+// // console.log(moreNumbers);
+
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
+
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
+
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
+
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Max', {moreDetail: []}];
+
+// const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+
+// for (const data of analyticsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
+
+// console.log(personalData[1]);
+
+const hobbies = ['sports', 'gaming'];
+
+// hobbies.push('cooking');
+// hobbies.unshift('cdoding');
+
+// hobbies.pop();
+// hobbies.shift();
+
+// hobbies[1] = 'coding';
+// hobbies[5] = 'music';
+
+hobbies.splice(1, 0, 'Good food');
+console.log(hobbies);
