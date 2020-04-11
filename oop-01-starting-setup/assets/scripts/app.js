a9ee4a1e62c6910002e3b70b2b9e3d1d9c5439fd
Asgrim_S <jan.semik@gmail.com>
2020-04-11 13:38:33 +0200

    oop wip

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,49 +1,66 @@
-const productList = {
-	products: [
-		{
-			title: 'Pillow',
-			imageUrl:
-				'https://media.sheridanoutlet.com.au/catalog/product/cache/1/image/1200x/17f82f742ffe127f42dca9de82fb58b1/1/7/1700x1700_fresh-loft-pillow-white.jpg?impolicy=original',
-			price: 19.99,
-			description: 'A soft pillow!',
-		},
-		{
-			title: 'Carpet',
-			imageUrl:
-				'https://kilimmalatya.com/content/images/thumbs/0001160_sivas-prison-carpet.jpeg',
-			price: 89.99,
-			description: 'A colorfull carpet!',
-		},
-		{
-			title: 'Chair',
-			imageUrl:
-				'https://cdn-images.article.com/products/SKU238/2890x1500/image42441.jpg?fit=max&w=1920&q=50',
-			price: 30,
-			description: 'A comfortable chair!',
-		},
-	],
-	redner() {
-		const renderHook = document.getElementById('app');
-		const prodList = document.createElement('ul');
-		prodList.className = 'product-list';
-		for (const prod of this.products) {
-			const prodEl = document.createElement('li');
-			prodEl.className = 'product-item';
-			prodEl.innerHTML = `
-            <div>
-                <img src="${prod.imageUrl}" alt="${prod.title}">
-                <div class="product-item__content">
-                <h2>${prod.title}</h2>
-                <h3>\$${prod.price}</h3>
-                <p>${prod.description}</p>
-                <button>Add to Cart</button>
-                </div>
-            </div>
-            `;
-			prodList.append(prodEl);
-		}
-		renderHook.append(prodList);
-	},
-};
-
-productList.redner();
+class Product {
+	constructor(title, image, desc, price) {
+	  this.title = title;
+	  this.imageUrl = image;
+	  this.description = desc;
+	  this.price = price;
+	}
+  }
+  
+  class ProductItem {
+	constructor(product) {
+	  this.product = product;
+	}
+  
+	render() {
+	  const prodEl = document.createElement('li');
+	  prodEl.className = 'product-item';
+	  prodEl.innerHTML = `
+		  <div>
+			<img src="${this.product.imageUrl}" alt="${this.product.title}" >
+			<div class="product-item__content">
+			  <h2>${this.product.title}</h2>
+			  <h3>\$${this.product.price}</h3>
+			  <p>${this.product.description}</p>
+			  <button>Add to Cart</button>
+			</div>
+		  </div>
+		`;
+	  return prodEl;
+	}
+  }
+  
+  class ProductList {
+	products = [
+	  new Product(
+		'A Pillow',
+		'https://media.sheridanoutlet.com.au/catalog/product/cache/1/image/1200x/17f82f742ffe127f42dca9de82fb58b1/1/7/1700x1700_fresh-loft-pillow-white.jpg?impolicy=original',
+		'A soft pillow!',
+		19.99
+	  ),
+	  new Product(
+		'A Carpet',
+		'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+		'A carpet which you might like - or not.',
+		89.99
+	  )
+	];
+  
+	constructor() {}
+  
+	render() {
+	  const renderHook = document.getElementById('app');
+	  const prodList = document.createElement('ul');
+	  prodList.className = 'product-list';
+	  for (const prod of this.products) {
+		const productItem = new ProductItem(prod);
+		const prodEl = productItem.render();
+		prodList.append(prodEl);
+	  }
+	  renderHook.append(prodList);
+	}
+  }
+  
+  const productList = new ProductList();
+  productList.render();
+  
